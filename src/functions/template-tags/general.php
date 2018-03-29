5b44913be851b1fcd3deb6eb1e00ad85273f39bd
jesseeproductions <info@jesseeproductions.com>
2018-03-29 10:15:03 -0400

    save dynamic code on save, store old code, update code on changes, display code in admin

--- src/functions/template-tags/general.php
+++ src/functions/template-tags/general.php
@@ -12,23 +12,50 @@ if ( ! class_exists( 'Pngx__Main' ) ) {
 	return;
 }
 
-/**
- * Sanitize URL for display on Front End
- *
- * @param $url
- *
- * @return mixed
- */
-function pngx_sanitize_url( $url ) {
+if ( ! function_exists( 'pngx_sanitize_url' ) ) {
+	/**
+	 * Sanitize URL for display on Front End
+	 *
+	 * @param $url
+	 *
+	 * @return mixed
+	 */
+	function pngx_sanitize_url( $url ) {
 
-	$encode = array( '&amp;', '&#038;', '&' );
-	$replacement = '%%PNGXAMP%%';
-	$sanitized_url = $url;
+		$encode        = array( '&amp;', '&#038;', '&' );
+		$replacement   = '%%PNGXAMP%%';
+		$sanitized_url = $url;
 
-	//replace ampersand before escaping to prevent breaking url
-	$sanitized_url = str_replace( $encode, $replacement, $sanitized_url );
-	$sanitized_url = esc_url( $sanitized_url );
+		//replace ampersand before escaping to prevent breaking url
+		$sanitized_url = str_replace( $encode, $replacement, $sanitized_url );
+		$sanitized_url = esc_url( $sanitized_url );
 
-	//add back ampersand after escaping
-	return str_replace( $replacement, '&', $sanitized_url );
+		//add back ampersand after escaping
+		return str_replace( $replacement, '&', $sanitized_url );
+	}
+}
+
+
+if ( ! function_exists( 'pngx_detect_change' ) ) {
+	/**
+	 * Detect Changes to Meta Settings
+	 *
+	 * @param $_POST_arr
+	 * @param $post_id
+	 * @param $meta_key
+	 * @param $post_key
+	 *
+	 * @return bool
+	 */
+	function pngx_detect_change( $_POST_arr, $post_id, $meta_key, $post_key ) {
+
+		$current_val = get_post_meta( $post_id, $meta_key, true );
+		$updated_val = ! empty( $_POST_arr[$post_key] ) ? esc_attr( $_POST_arr[$post_key] ) : null;
+
+		if ( $current_val !== $updated_val ) {
+			return true;
+		}
+
+		return false;
+	}
 }
