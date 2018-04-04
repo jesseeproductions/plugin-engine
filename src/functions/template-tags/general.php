d750fe1353eb27b6f44df1953987d8f36c1df9cb
jesseeproductions <info@jesseeproductions.com>
2018-04-04 11:08:00 -0400

    reveal and dynamic code saved to cookie

--- src/functions/template-tags/general.php
+++ src/functions/template-tags/general.php
@@ -59,3 +59,37 @@ if ( ! function_exists( 'pngx_detect_change' ) ) {
 		return false;
 	}
 }
+
+if ( ! function_exists( 'pngx_wp_strtotime' ) ) {
+	/**
+	 * Set Time Based off WordPress Timezone
+	 *
+	 * https://gist.github.com/anthonyeden/0cf8eb86f7e634b3d5ded4debc59cb84#file-wordpress-strtotime-php
+	 *
+	 * @param $str
+	 *
+	 * @return string
+	 */
+	function pngx_wp_strtotime( $str ) {
+		// This function behaves a bit like PHP's StrToTime() function, but taking into account the Wordpress site's timezone
+		// CAUTION: It will throw an exception when it receives invalid input - please catch it accordingly
+		// From https://mediarealm.com.au/
+		$tz_string = get_option( 'timezone_string' );
+		$tz_offset = get_option( 'gmt_offset', 0 );
+		if ( ! empty( $tz_string ) ) {
+			// If site timezone option string exists, use it
+			$timezone = $tz_string;
+		} elseif ( $tz_offset == 0 ) {
+			// get UTC offset, if it isn’t set then return UTC
+			$timezone = 'UTC';
+		} else {
+			$timezone = $tz_offset;
+			if ( substr( $tz_offset, 0, 1 ) != "-" && substr( $tz_offset, 0, 1 ) != "+" && substr( $tz_offset, 0, 1 ) != "U" ) {
+				$timezone = "+" . $tz_offset;
+			}
+		}
+		$datetime = new DateTime( $str, new DateTimeZone( $timezone ) );
+
+		return $datetime->format( 'U' );
+	}
+}
