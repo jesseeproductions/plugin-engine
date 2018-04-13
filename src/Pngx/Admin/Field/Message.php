a8fab8f54cbedf91a0ee0b95e028cce9c5357450
jesseeproductions <info@jesseeproductions.com>
2018-04-13 16:32:35 -0400

    update messages and style dynamic color on display

--- src/Pngx/Admin/Field/Message.php
+++ src/Pngx/Admin/Field/Message.php
@@ -16,7 +16,9 @@ class Pngx__Admin__Field__Message {
 
 	public static function display( $field = array(), $options = array(), $options_id = null, $meta = null ) {
 
-		echo '<div class="pngx-message-field">' . strip_tags( $field['desc'], apply_filters( 'cctor_filter_terms_tags', '' ) ) . '</div>';
+		$attributes = empty( $field['field_attributes'] ) ? '' : Pngx__Admin__Field_Methods::instance()->set_field_attributes( $field['field_attributes'] );
+
+		echo '<div class="pngx-message-field" ' . $attributes . '>' . strip_tags( $field['desc'], apply_filters( 'cctor_filter_terms_tags', '' ) ) . '</div>';
 
 	}
 
