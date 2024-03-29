d6862675f7e11d7c79b2cde76393a7c2807fcc37
jesseeproductions <info@jesseeproductions.com>
2024-03-29 15:16:57 -0400

    Add placeholder to number field to prevent notices

--- src/Pngx/Admin/Field/V2/Number.php
+++ src/Pngx/Admin/Field/V2/Number.php
@@ -35,28 +35,29 @@ class Number {
 		$field_wrap = isset( $field['fieldset_wrap'] ) ? $field['fieldset_wrap'] : [];
 
 		$template->template( 'components/field', [
-				'classes_wrap'   => [ "pngx-engine-field__{$field['id']}-wrap", ...$field_wrap ],
-				'id'             => $field['id'],
-				'label'          => $field['label'],
-				'tooltip'        => $field['tooltip'] ?? null,
-				'fieldset_attrs' => ! empty( $field['fieldset_attrs'] ) ? (array) $field['fieldset_attrs'] : [],
-				'template_name'  => 'number',
-				'template_echo'  => true,
-				'template_args'  => [
-					'id'            => $field['id'],
-					'label'         => $field['label'],
-					'description'   => ! empty( $field['description'] ) ? $field['description'] : '',
-					'classes_wrap'  => ! empty( $field['classes_wrap'] ) ? (array) $field['classes_wrap'] : [],
-					'classes_input' => ! empty( $field['classes_input'] ) ? (array) $field['classes_input'] : [ 'pngx-meta-field' ],
-					'classes_label' => ! empty( $field['classes_label'] ) ? (array) $field['classes_label'] : [ 'screen-reader-text' ],
-					'name'          => $name,
-					'value'         => $value,
-					'min'           => isset( $field['min'] ) ? $field['min'] : '',
-					'max'           => isset( $field['max'] ) ? $field['max'] : '',
-					'step'          => isset( $field['step'] ) ? $field['step'] : '',
-					'attrs'         => ! empty( $field['attrs'] ) ? (array) $field['attrs'] : [],
-					'wrap_attrs'    => ! empty( $field['wrap_attrs'] ) ? (array) $field['wrap_attrs'] : [],
-				],
-			] );
+			'classes_wrap'   => [ "pngx-engine-field__{$field['id']}-wrap", ...$field_wrap ],
+			'id'             => $field['id'],
+			'label'          => $field['label'],
+			'tooltip'        => $field['tooltip'] ?? null,
+			'fieldset_attrs' => ! empty( $field['fieldset_attrs'] ) ? (array) $field['fieldset_attrs'] : [],
+			'template_name'  => 'number',
+			'template_echo'  => true,
+			'template_args'  => [
+				'id'            => $field['id'],
+				'label'         => $field['label'],
+				'description'   => ! empty( $field['description'] ) ? $field['description'] : '',
+				'classes_wrap'  => ! empty( $field['classes_wrap'] ) ? (array) $field['classes_wrap'] : [],
+				'classes_input' => ! empty( $field['classes_input'] ) ? (array) $field['classes_input'] : [ 'pngx-meta-field' ],
+				'classes_label' => ! empty( $field['classes_label'] ) ? (array) $field['classes_label'] : [ 'screen-reader-text' ],
+				'name'          => $name,
+				'value'         => $value,
+				'min'           => isset( $field['min'] ) ? $field['min'] : '',
+				'max'           => isset( $field['max'] ) ? $field['max'] : '',
+				'step'          => isset( $field['step'] ) ? $field['step'] : '',
+				'placeholder'   => isset( $field['placeholder'] ) ? $field['placeholder'] : '',
+				'attrs'         => ! empty( $field['attrs'] ) ? (array) $field['attrs'] : [],
+				'wrap_attrs'    => ! empty( $field['wrap_attrs'] ) ? (array) $field['wrap_attrs'] : [],
+			],
+		] );
 	}
 }
