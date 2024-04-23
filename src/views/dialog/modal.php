700a3cf9fe6c64d8d3ce0db91dcedb01fbff52dc
jesseeproductions <info@jesseeproductions.com>
2024-04-23 15:58:45 -0400

    Starter Plugin updates

--- src/views/dialog/modal.php
+++ src/views/dialog/modal.php
@@ -17,12 +17,11 @@ $dialog_view = pngx( 'pngx.dialog.view' );
 $vars        = get_defined_vars();
 ?>
 <?php $dialog_view->template( 'button', $vars, true ); ?>
-<script data-js="<?php echo esc_attr( 'dialog-content-' . $id ); ?>" type="text/template">
+<template data-js="<?php echo esc_attr( 'dialog-content-' . $id ); ?>" type="text/template">
 	<?php if ( ! empty( $title ) ) : ?>
 		<h2 <?php pngx_classes( $title_classes ) ?>><?php echo esc_html( $title ); ?></h2>
 	<?php endif; ?>
 	<div <?php pngx_classes( $content_classes ) ?>>
 		<?php echo $content; ?>
 	</div>
-</script>
-
+</template>
